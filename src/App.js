3ef3396179f6b693c1570fb8910b9c06368daa0c
Aleksi Peiponen <aleksi.peiponen@mediasignal.fi>
2019-02-21 13:39:04 +0200

    Added theme provider and first base for default theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import './App.css'
 import { IntlProvider, FormattedMessage } from 'react-intl';
+import { MuiThemeProvider, Typography } from '@material-ui/core';
+
+import './App.css'
 import i18n from './i18n';
+import themes from './themes';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,9 +19,13 @@ class App extends React.Component {
     const i18nData = i18n.data();
     return (
       <IntlProvider {...i18nData}>
-        <div className="App">
-          <FormattedMessage id="app.title" />
-        </div>
+        <MuiThemeProvider theme={themes.SMTheme}>
+          <div className="App">
+            <Typography variant={'p'}>
+              <FormattedMessage id="app.title" />
+            </Typography>
+          </div>
+        </MuiThemeProvider>
       </IntlProvider>
     );
   }
