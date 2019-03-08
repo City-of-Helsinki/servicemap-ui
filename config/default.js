1601395159a317661fa5672531a0adb203e61267
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-03-08 13:09:21 +0200

    Add unit handler middleware to server
    Move language handler to server utils

--- config/default.js
+++ config/default.js
@@ -1,10 +1,14 @@
 export default {
-    "server": {
-        "address": "127.0.0.1",
-        "port": "2048",
-    },
-    "street_address_languages": ["fi", "sv"],
-    "supported_languages": [
-        "fi", "sv", "en"
-    ],
+  "server": {
+    "address": "127.0.0.1",
+    "port": "2048",
+    "url_prefix": "/"
+  },
+  "street_address_languages": ["fi", "sv"],
+  "supported_languages": [
+    "fi", "sv", "en"
+  ],
+  "unit": {
+    "api_url": "https://api.hel.fi/servicemap/v2/unit/"
+  }
 }
