3aedb0c31e97df7564bbb57e7d2ec59fce68a413
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-03-21 12:32:59 +0200

    Improve search list performance
    Change SearchList to update only when data changes

--- src/views/SearchView/components/SearchList.js
+++ src/views/SearchView/components/SearchList.js
@@ -18,43 +18,49 @@ const styles = {
   },
 };
 
-const SearchList = (props) => {
-  const {
-    data, match, classes,
-  } = props;
-  const { params } = match;
-  const lng = params && params.lng;
+class SearchList extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    const { data } = this.props;
+    return (data !== nextProps.data);
+  }
 
-  return (
-    <div className="SearchList">
-      <List className={classes.list}>
-        {
-          data && data.length
-          // TODO: Check duplicate keys error with big datasets
-          && data.map((item) => {
-            const { id } = item;
-            if (item) {
-              return (
-                <SearchItem
-                  key={`${id}`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    const { history } = props;
-                    if (history) {
-                      history.push(`/${lng || 'fi'}/unit/${id}/`);
-                    }
-                  }}
-                  data={item}
-                />
-              );
-            }
-            return null;
-          })
-        }
-      </List>
-    </div>
-  );
-};
+  render() {
+    const {
+      data, match, classes, history,
+    } = this.props;
+    const { params } = match;
+    const lng = params && params.lng;
+
+    return (
+      <div className="SearchList">
+        <List className={classes.list}>
+          {
+            data && data.length
+            // TODO: Check duplicate keys error with big datasets
+            && data.map((item) => {
+              const { id } = item;
+              if (item) {
+                return (
+                  <SearchItem
+                    key={`${id}`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      if (history) {
+                        history.push(`/${lng || 'fi'}/unit/${id}/`);
+                      }
+                    }}
+                    data={item}
+                  />
+                );
+              }
+              return null;
+            })
+          }
+        </List>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(withRouter(SearchList));
 
@@ -62,6 +68,7 @@ export default withStyles(styles)(withRouter(SearchList));
 SearchList.propTypes = {
   classes: PropTypes.objectOf(PropTypes.any),
   data: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)),
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
   match: PropTypes.objectOf(PropTypes.any),
 };
 
