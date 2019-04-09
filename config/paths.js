dabcb68879a382ce30066e280429dad17442fcc4
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-04-09 12:31:40 +0300

    Change path object's structure

--- config/paths.js
+++ config/paths.js
@@ -2,15 +2,21 @@ import config from './index';
 
 // Application paths
 const paths = {
-  home: () => '/',
-  search: query => `/search${query ? `?q=${query}` : ''}`,
-  unit: id => `/unit/${id || ''}`
+  home: {
+    generate: () => '/',
+  },
+  search: {
+    generate: query => `/search${query ? `?q=${query}` : ''}`,
+  },
+  unit: {
+    generate: id => `/unit/${id || ''}`,
+  },
 }
 
 // Generate path for page
 export const generatePath = (path, locale = config.default_locale, data = null) => {
   if (paths.hasOwnProperty(path)) {
-    const pathString = paths[path](data); // Create path string
+    const pathString = paths[path].generate(data); // Create path string
     return `/${locale || config.default_locale}${pathString}`; // Return path with locale
   }
   return;
