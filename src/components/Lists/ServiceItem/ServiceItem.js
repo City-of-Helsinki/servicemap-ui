13fb8efd34c16e4ced47076a401eb94ce1106dcd
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-04-16 15:07:04 +0300

    Change ServiceItem and UnitItem to draw icons on componentDidMount
    Drawing icon on componentDidMount avoids problem with canvas not existing on server side

--- src/components/Lists/ServiceItem/ServiceItem.js
+++ src/components/Lists/ServiceItem/ServiceItem.js
@@ -8,29 +8,40 @@ import { generatePath } from '../../../utils/path';
 import { getLocaleString } from '../../../redux/selectors/locale';
 import { uppercaseFirst } from '../../../utils';
 
-const ServiceItem = (props) => {
-  const {
-    service, getLocaleText, history, match,
-  } = props;
+class ServiceItem extends React.Component {
+  state = {
+    icon: <img alt="" src={null} style={{ height: 24 }} aria-hidden="true" />,
+  };
 
-  const { params } = match;
-  const lng = params && params.lng;
-  return (
-    <SimpleListItem
-      button
-      text={uppercaseFirst(getLocaleText(service.name))}
-      icon={<img alt="" src={drawServiceIcon()} style={{ height: 24 }} aria-hidden="true" />}
-      handleItemClick={(e) => {
-        e.preventDefault();
-        if (history) {
-          history.push(generatePath('service', lng, service.id));
-        }
-      }}
-      role="link"
-      component="li"
-    />
-  );
-};
+  componentDidMount() {
+    this.setState({ icon: <img alt="" src={drawServiceIcon()} style={{ height: 24 }} aria-hidden="true" /> });
+  }
+
+  render() {
+    const {
+      service, getLocaleText, history, match,
+    } = this.props;
+    const { icon } = this.state;
+
+    const { params } = match;
+    const lng = params && params.lng;
+    return (
+      <SimpleListItem
+        button
+        text={uppercaseFirst(getLocaleText(service.name))}
+        icon={icon}
+        handleItemClick={(e) => {
+          e.preventDefault();
+          if (history) {
+            history.push(generatePath('service', lng, service.id));
+          }
+        }}
+        role="link"
+        component="li"
+      />
+    );
+  }
+}
 
 // Listen to redux state
 const mapStateToProps = (state) => {
--- src/components/Lists/UnitItem/UnitItem.js
+++ src/components/Lists/UnitItem/UnitItem.js
@@ -10,60 +10,77 @@ import ResultItem from '../../ResultList/ResultItem';
 import { drawIcon } from '../../../views/Map/utils/drawIcon';
 import { getLocaleString } from '../../../redux/selectors/locale';
 
-const UnitItem = ({
-  unit, onClick, getLocaleText, history, intl, listId, match,
-}) => {
-  // Don't render if not valid unit
-  if (!isValidUnit(unit)) {
-    return null;
+class UnitItem extends React.Component {
+  state = {
+    icon: <img alt="" src={null} style={{ height: 24 }} aria-hidden="true" />,
+  };
+
+  componentDidMount() {
+    const { unit } = this.props;
+    this.setState({ icon: <img alt="" src={drawIcon(unit, null, true)} style={{ height: 24 }} aria-hidden="true" /> });
   }
 
-  const {
-    id, name, object_type,
-  } = unit;
-
-  const { params } = match;
-  const lng = params && params.lng;
-
-  // Accessibility text
-  // TODO: Change to check data once accessibility messages functionality has been added
-  // TODO: Change texts to use translations once data is accessible
-  const accessibilityProblems = null;
-  let accessText = intl.formatMessage({ id: 'unit.accessibility.noInfo' });
-  if (accessibilityProblems !== null && typeof accessibilityProblems !== 'undefined') {
-    switch (accessibilityProblems) {
-      case 0:
-        accessText = intl.formatMessage({ id: 'unit.accessibility.ok' });
-        break;
-      default:
-        accessText = intl.formatMessage({ id: 'unit.accessibility.problems' }, { count: accessibilityProblems });
+  render() {
+    const {
+      unit, onClick, getLocaleText, history, intl, listId, match,
+    } = this.props;
+    // Don't render if not valid unit
+    if (!isValidUnit(unit)) {
+      return null;
     }
+
+    // Get icon
+    const { icon } = this.state;
+
+    // Parse unit data
+    const {
+      id, name, object_type,
+    } = unit;
+
+    // Prase language params
+    const { params } = match;
+    const lng = params && params.lng;
+
+    // Accessibility text
+    // TODO: Change to check data once accessibility messages functionality has been added
+    // TODO: Change texts to use translations once data is accessible
+    const accessibilityProblems = null;
+    let accessText = intl.formatMessage({ id: 'unit.accessibility.noInfo' });
+    if (accessibilityProblems !== null && typeof accessibilityProblems !== 'undefined') {
+      switch (accessibilityProblems) {
+        case 0:
+          accessText = intl.formatMessage({ id: 'unit.accessibility.ok' });
+          break;
+        default:
+          accessText = intl.formatMessage({ id: 'unit.accessibility.problems' }, { count: accessibilityProblems });
+      }
+    }
+
+    // Distance text
+    // TODO: Change to check data for distance once location info is available
+    const distance = null; // '100';
+
+    return (
+      <ResultItem
+        title={getLocaleText(name)}
+        subtitle={intl.formatMessage({ id: object_type })}
+        botRightText={accessText}
+        distancePosition={distance}
+        icon={icon}
+        itemId={id}
+        listId={listId}
+        onClick={(e) => {
+          e.preventDefault();
+          if (onClick) {
+            onClick();
+          } else if (history) {
+            history.push(generatePath('unit', lng, id));
+          }
+        }}
+      />
+    );
   }
-
-  // Distance text
-  // TODO: Change to check data for distance once location info is available
-  const distance = null; // '100';
-
-  return (
-    <ResultItem
-      title={getLocaleText(name)}
-      subtitle={intl.formatMessage({ id: object_type })}
-      botRightText={accessText}
-      distancePosition={distance}
-      icon={<img alt="" src={drawIcon(unit, null, true)} style={{ height: 24 }} aria-hidden="true" />}
-      itemId={id}
-      listId={listId}
-      onClick={(e) => {
-        e.preventDefault();
-        if (onClick) {
-          onClick();
-        } else if (history) {
-          history.push(generatePath('unit', lng, id));
-        }
-      }}
-    />
-  );
-};
+}
 
 // Listen to redux state
 const mapStateToProps = (state) => {
