53d2e42670aec4c239bddbc41217e72dce1b0831
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-04-18 14:30:15 +0300

    Fix search view live area

--- src/views/SearchView/SearchView.js
+++ src/views/SearchView/SearchView.js
@@ -55,27 +55,6 @@ class SearchView extends React.Component {
 
     return (
       <div className="Search">
-        {
-          // Screen reader only information
-        }
-        <Typography variant="srOnly">
-          {
-            isFetching && max === 0
-            && <FormattedMessage id="search.started" />
-          }
-        </Typography>
-        <Typography variant="srOnly">
-          {
-            isFetching && max > 0
-              && <FormattedMessage id="search.loading.units.srInfo" values={{ count: max }} />
-          }
-        </Typography>
-        <Typography variant="srOnly">
-          {
-            !isFetching
-            && <FormattedMessage id="search.info" values={{ count: unitCount }} />
-          }
-        </Typography>
         <SearchBar
           searchRef={this.searchField}
           onSubmit={this.onSearchSubmit}
@@ -87,7 +66,27 @@ class SearchView extends React.Component {
             isFetching
             && <Loading text={intl && intl.formatMessage({ id: 'search.loading.units' }, { count, max })} progress={progress} />
           }
-
+          {
+            // Screen reader only information
+          }
+          <Typography variant="srOnly">
+            {
+              isFetching && max === 0
+              && <FormattedMessage id="search.started" />
+            }
+          </Typography>
+          <Typography variant="srOnly">
+            {
+              isFetching && max > 0
+                && <FormattedMessage id="search.loading.units.srInfo" values={{ count: max }} />
+            }
+          </Typography>
+          <Typography variant="srOnly">
+            {
+              !isFetching
+              && <FormattedMessage id="search.info" values={{ count: unitCount }} />
+            }
+          </Typography>
         </Paper>
         {
           resultsShowing
