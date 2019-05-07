c9144ef0127d85568deff08c7664952b740dbe0d
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-05-07 12:24:20 +0300

    New focusToViewTitle utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,12 @@
 const isClient = () => typeof window !== 'undefined';
 
-const tAttr = (attr) => {
-  let lang; let
-    len;
 
-  if (!attr) {
-    return attr;
+// Focus user to view title's link element
+export const focusToViewTitle = () => {
+  const elem = document.getElementById('view-title');
+  if (elem) {
+    elem.focus();
   }
-
-  if (!(attr instanceof Object)) {
-    console.error("translated attribute didn't get a translation object", attr);
-    return attr;
-  }
-
-  // Try primary choice first, fallback to whatever's available.
-  const languages = [].concat(SUPPORTED_LANGUAGES);
-
-  for (let i = 0, len = languages.length; i < len; i++) {
-    lang = languages[i];
-    if (lang in attr) {
-      return attr[lang];
-    }
-  }
-
-  console.error('no supported languages found', attr);
-
-  return null;
 };
 
 export const uppercaseFirst = val => val.charAt(0).toUpperCase() + val.slice(1);
