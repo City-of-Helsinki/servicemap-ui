a7bb8072a388faefefa5f72e80c86fa1d7cdfb73
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-05-14 14:20:25 +0300

    Fix MobileMapView merge conflicts

--- src/views/MobileMapView/MobileMapView.js
+++ src/views/MobileMapView/MobileMapView.js
@@ -1,33 +1,30 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from 'react-intl';
 import { connect } from 'react-redux';
 import SearchBar from '../../components/SearchBar';
 import HomeView from '../HomeView';
 import { DesktopComponent, MobileComponent } from '../../layouts/WrapperComponents/WrapperComponents';
 
-class MobileMapView extends React.Component {
-
-  render() {
-    const { intl, map } = this.props;
-    if (map && typeof window !== 'undefined') {
-      // Reftesh map when changing mobile page initialize correct size
-      map._onResize();
-    }
-    return (
-      <>
-        <MobileComponent>
-          <SearchBar
-            placeholder={intl.formatMessage({ id: 'search' })}
-          />
-        </MobileComponent>
-        <DesktopComponent>
-          <HomeView />
-        </DesktopComponent>
-      </>
-    );
+const MobileMapView = ({ intl, map }) => {
+  if (map && typeof window !== 'undefined') {
+    // Reftesh map when changing mobile page initialize correct size
+    map._onResize();
   }
-}
+  return (
+    <>
+      <MobileComponent>
+        <SearchBar
+          placeholder={intl.formatMessage({ id: 'search' })}
+        />
+      </MobileComponent>
+      <DesktopComponent>
+        <HomeView />
+      </DesktopComponent>
+    </>
+  );
+};
 
 const mapStateToProps = state => ({
   map: state.mapRef.leafletElement,
@@ -36,7 +33,6 @@ const mapStateToProps = state => ({
 
 MobileMapView.propTypes = {
   intl: intlShape.isRequired,
-  match: PropTypes.objectOf(PropTypes.any).isRequired,
   map: PropTypes.objectOf(PropTypes.any),
 };
 
