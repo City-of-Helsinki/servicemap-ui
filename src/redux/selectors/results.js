54346c9c4c1d383df04f118b90afb88c455c5a41
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-05-20 12:00:51 +0300

    Refactor sorter function

--- src/redux/selectors/results.js
+++ src/redux/selectors/results.js
@@ -3,31 +3,29 @@ import { getLocaleString } from './locale';
 export const getOrderedData = (state, data, direction, order) => {
   const results = Array.from(data);
 
-  // Alphabetical ordering
-  if (order === 'alphabetical') {
-    results.sort((a, b) => {
-      const x = getLocaleString(state, a.name).toLowerCase();
-      const y = getLocaleString(state, b.name).toLowerCase();
-      if (x > y) { return -1; }
-      if (x < y) { return 1; }
-      return 0;
-    });
+  switch (order) {
+    // Alphabetical ordering
+    case 'alphabetical': {
+      results.sort((a, b) => {
+        const x = getLocaleString(state, a.name).toLowerCase();
+        const y = getLocaleString(state, b.name).toLowerCase();
+        if (x > y) { return -1; }
+        if (x < y) { return 1; }
+        return 0;
+      });
 
-    if (direction === 'desc') {
-      results.reverse();
-    }
-  } else {
-    // Ordering based on match score
-    results.sort((a, b) => {
-      switch (order) {
-        case 'match': {
-          return direction === 'asc' ? a.score - b.score : b.score - a.score;
-        }
-        default: {
-          return 0;
-        }
+      // If reversed alphabetical ordering
+      if (direction === 'desc') {
+        results.reverse();
       }
-    });
+      break;
+    }
+    // Ordering based on match score
+    case 'match': {
+      results.sort((a, b) => (direction === 'asc' ? a.score - b.score : b.score - a.score));
+      break;
+    }
+    default:
   }
 
   return results;
