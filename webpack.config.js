99bd7959ce317fdd6b3c0b3472534139c033e4d9
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-06-12 21:23:50 +0300

    Refactor webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,34 @@ const isEnvProduction = NODE_ENV === 'production';
 const isEnvDevelopment = !isEnvProduction;
 
 const js = {
-  test: /\.js$/,
+  
+  test: /\.(js|mjs|jsx|ts|tsx)$/,
   exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime'],
-    },
-  },
+  loader: require.resolve('babel-loader'),
+  options: {
+    customize: require.resolve(
+      'babel-preset-react-app/webpack-overrides'
+    ),
+    
+    plugins: [
+      [
+        require.resolve('babel-plugin-named-asset-import'),
+        {
+          loaderMap: {
+            svg: {
+              ReactComponent: '@svgr/webpack?-svgo,+ref![path]',
+            },
+          },
+        },
+      ],
+    ],
+    // This is a feature of `babel-loader` for webpack (not Babel itself).
+    // It enables caching results in ./node_modules/.cache/babel-loader/
+    // directory for faster rebuilds.
+    cacheDirectory: true,
+    cacheCompression: isEnvProduction,
+    compact: isEnvProduction,
+  }
 };
 
 const fonts = (isClient = true) => ({
@@ -56,26 +75,8 @@ const serverConfig = {
     rules: [
       {
         oneOf: [
-          // Process application JS with Babel.
-          // The preset includes JSX, Flow, TypeScript, and some ESnext features.
-          {
-            test: /\.(js|mjs|jsx|ts|tsx)$/,
-            include: /src/,
-            loader: require.resolve('babel-loader'),
-            options: {
-              customize: require.resolve(
-                'babel-preset-react-app/webpack-overrides'
-              ),
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-              cacheCompression: isEnvProduction,
-              compact: isEnvProduction,
-            },
-          },
-          fonts(false),
           js,
+          fonts(false),
           css
         ],
       },
@@ -100,53 +101,6 @@ const clientConfig = {
           js,
           fonts(),
           css,
-          // Process application JS with Babel.
-          // The preset includes JSX, Flow, TypeScript, and some ESnext features.
-          {
-            test: /\.(js|mjs|jsx|ts|tsx)$/,
-            include: /src/,
-            loader: require.resolve('babel-loader'),
-            options: {
-              customize: require.resolve(
-                'babel-preset-react-app/webpack-overrides'
-              ),
-              
-              plugins: [
-                [
-                  require.resolve('babel-plugin-named-asset-import'),
-                  {
-                    loaderMap: {
-                      svg: {
-                        ReactComponent: '@svgr/webpack?-svgo,+ref![path]',
-                      },
-                    },
-                  },
-                ],
-              ],
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-              cacheCompression: isEnvProduction,
-              compact: isEnvProduction,
-            },
-          },
-          // "file" loader makes sure those assets get served by WebpackDevServer.
-          // When you `import` an asset, you get its (virtual) filename.
-          // In production, they would get copied to the `build` folder.
-          // This loader doesn't use a "test" so it will catch all modules
-          // that fall through the other loaders.
-          {
-            loader: require.resolve('file-loader'),
-            // Exclude `js` files to keep "css" loader working as it injects
-            // its runtime that would otherwise be processed through "file" loader.
-            // Also exclude `html` and `json` extensions so they get processed
-            // by webpacks internal loaders.
-            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-            options: {
-              name: '/static/media/[name].[hash:8].[ext]',
-            },
-          },
         ],
       }
     ],
