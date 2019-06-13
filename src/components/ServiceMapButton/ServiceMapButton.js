083ce7e1846bd51e723c8fb235442f6d24bdb1b5
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-06-13 13:11:03 +0300

    Update Container and ServiceMapButton components to accept additional parameters

--- src/components/ServiceMapButton/ServiceMapButton.js
+++ src/components/ServiceMapButton/ServiceMapButton.js
@@ -4,15 +4,16 @@ import { Button, withStyles } from '@material-ui/core';
 
 
 const BlueButton = ({
-  onClick, srText, classes, children,
+  children, classes, className, onClick, srText, style,
 }) => (
   <Button
-    className={classes.button}
+    className={`${classes.button} ${className}`}
     role="link"
     variant="contained"
     color="primary"
     onClick={onClick}
     aria-label={srText}
+    style={style}
   >
     {children}
   </Button>
@@ -29,14 +30,18 @@ const styles = () => ({
 });
 
 BlueButton.propTypes = {
+  className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
   srText: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.any),
   classes: PropTypes.objectOf(PropTypes.any).isRequired,
   children: PropTypes.node.isRequired,
 };
 
 BlueButton.defaultProps = {
+  className: '',
   srText: null,
+  style: null,
 };
 
 export default withStyles(styles)(BlueButton);
