00e9fcb5a95e97b1a590731b68b8dcdfe4097d48
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-09-13 13:09:30 +0300

    Refactor TopBar to use class stylings

--- src/components/TopBar/styles.js
+++ src/components/TopBar/styles.js
@@ -12,9 +12,30 @@ const styles = () => ({
     display: 'flex',
     flexDirection: 'column',
   },
+  buttonSettings: {
+    backgroundColor: '#4e67d2',
+  },
+  buttonSettingsIcon: {
+    marginRight: 8,
+  },
+  buttonMap: {
+    backgroundColor: '#4e67d2',
+  },
   desktopNav: {
     position: 'relative',
     height: topBarHeight,
+  },
+  feedbackLink: {
+    display: 'inline-block',
+  },
+  feedbackText: {
+    margin: 0,
+    color: 'white',
+    textDecorationColor: 'white',
+  },
+  homeLogoContainer: {
+    paddingTop: 8,
+    alignSelf: 'center',
   },
   languages: {
     flex: '0 0 auto',
@@ -22,6 +43,16 @@ const styles = () => ({
   },
   logo: {
     marginLeft: 8,
+  },
+  logoContainer: {
+    flex: '1 0 auto',
+    display: 'flex',
+  },
+  logoHomeLink: {
+    alignSelf: 'center',
+  },
+  noTextTransform: {
+    textTransform: 'none',
   },
   mobileNav: {
     position: 'sticky',
