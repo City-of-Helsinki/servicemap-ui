0499b2d5d3c3a6b4458a78f97cca4d31ae8f8feb
mikkoisa <mikko.saarinen@ceon.fi>
2019-10-07 12:53:58 +0300

    Add missing comment

--- src/i18n/translations/sv.js
+++ src/i18n/translations/sv.js
@@ -36,7 +36,7 @@ export default {
   'address.error': 'Osoitetta ei löytynyt', // TODO: Translate
   'address.nearby': 'Lähellä', // TODO: Translate
   'address.districts': 'Alueet', // TODO: Translate
-  'address.plural': 'Osoitteet',
+  'address.plural': 'Osoitteet', // TODO: Translate
 
   // Event
   'event.nearby': 'Nearby events', // TODO: Translate
