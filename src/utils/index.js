41f17d02a3d120a111b8624d6ff7462119422e93
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-10-15 15:52:37 +0300

    Fix stylings for ResultItem in small content areas

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable camelcase */
+import { unstable_useMediaQuery } from '@material-ui/core/useMediaQuery';
+import config from '../../config';
+
 const isClient = () => typeof window !== 'undefined';
 
 
@@ -113,6 +117,23 @@ export const valuesHaveChanged = (obj1, obj2, keys = []) => {
   });
 
   return hasChanged;
+};
+
+/**
+ * USE ONLY IN SIMPLE COMPONENTS because mediaquery hook
+ * Check if sidebar content is small
+ * Return true if smaller than smallContent treshold
+ * or smallscreen but not mobile
+ */
+export const isSmallContentArea = () => {
+  const { smallContentAreaBreakpoint, mobileUiBreakpoint, smallScreenBreakpoint } = config;
+  const smallContent = unstable_useMediaQuery(`(max-width:${smallContentAreaBreakpoint}px)`);
+  const smallScreen = unstable_useMediaQuery(`(max-width:${smallScreenBreakpoint}px)`);
+  const notMobile = unstable_useMediaQuery(`(min-width:${mobileUiBreakpoint}px)`);
+  return (
+    smallContent
+    || (smallScreen && notMobile)
+  );
 };
 
 
