ab1287f625666b5efae627498c3b4f1a9d52c792
Aleksi Peiponen <aleksi.laurea@gmail.com>
2019-10-30 15:21:53 +0200

    Fix server side fetch errors breaking site and giving 500 response

--- src/views/EventDetailView/index.js
+++ src/views/EventDetailView/index.js
@@ -1,19 +1,29 @@
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import { injectIntl } from 'react-intl';
 import EventDetailView from './EventDetailView';
+import { changeSelectedEvent } from '../../redux/actions/event';
+import { fetchSelectedUnit } from '../../redux/actions/selectedUnit';
 import { getLocaleString } from '../../redux/selectors/locale';
 import styles from './styles';
 
 const mapStateToProps = (state) => {
   const getLocaleText = textObject => getLocaleString(state, textObject);
+  const event = state.event.selected;
+  const selectedUnit = state.selectedUnit.unit.data;
+  const map = state.mapRef.leafletElement;
   const { navigator } = state;
   return {
+    event,
     getLocaleText,
     navigator,
+    map,
+    selectedUnit,
   };
 };
 
-export default withStyles(styles)(connect(
+export default withStyles(styles)(withRouter(injectIntl(connect(
   mapStateToProps,
-  null,
-)(EventDetailView));
+  { changeSelectedEvent, fetchSelectedUnit },
+)(EventDetailView))));
