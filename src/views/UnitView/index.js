2caf617fe42c9543ff8e2a79311dbac722d00059
mikkoisa <mikko.saarinen@ceon.fi>
2020-01-07 15:27:06 +0200

    Improve view lifecycle methods. Clean code

--- src/views/UnitView/index.js
+++ src/views/UnitView/index.js
@@ -4,9 +4,8 @@ import { withStyles } from '@material-ui/core';
 import { injectIntl } from 'react-intl';
 import { fetchSelectedUnit, changeSelectedUnit } from '../../redux/actions/selectedUnit';
 import { fetchAccessibilitySentences } from '../../redux/actions/selectedUnitAccessibility';
-import { fetchUnitEvents } from '../../redux/actions/event';
+import { fetchUnitEvents } from '../../redux/actions/selectedUnitEvents';
 import { fetchReservations } from '../../redux/actions/selectedUnitReservations';
-import { getSelectedUnit } from '../../redux/selectors/selectedUnit';
 import { getLocaleString } from '../../redux/selectors/locale';
 
 import UnitView from './UnitView';
@@ -14,27 +13,23 @@ import styles from './styles/styles';
 
 // Listen to redux state
 const mapStateToProps = (state) => {
-  const unit = getSelectedUnit(state);
+  const stateUnit = state.selectedUnit.unit.data;
   const unitFetching = state.selectedUnit.unit.isFetching;
-  const { accessibilitySentences } = state.selectedUnit;
-  const eventsData = state.event.data;
-  const eventFetching = state.event.isFetching;
+  const { accessibilitySentences, events, reservations } = state.selectedUnit;
   const getLocaleText = textObject => getLocaleString(state, textObject);
   const map = state.mapRef.leafletElement;
   const { navigator } = state;
-  const reservations = state.selectedUnit.reservations.data;
   const { user } = state;
 
   return {
     accessibilitySentences: accessibilitySentences.data,
-    unit,
+    stateUnit,
     unitFetching,
-    eventsData,
-    eventFetching,
+    eventsData: events,
     getLocaleText,
     map,
     navigator,
-    reservations,
+    reservationsData: reservations,
     userLocation: user.position.coordinates,
   };
 };
