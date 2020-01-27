95ab20cc9e15bfda43ef4b21358c1ed51399846d
AleksiP <aleksi.laurea@gmail.com>
2020-01-27 12:34:35 +0200

    Add production and development logos and change based on environment (#248)
    * Add production and development logos and change based on environment

--- server/server.js
+++ server/server.js
@@ -142,6 +142,7 @@ const htmlTemplate = (reactDom, preloadedState, css, jss, locale) => `
         window.nodeEnvSettings.RESERVATIONS_API = "${process.env.RESERVATIONS_API}";
         window.nodeEnvSettings.PRODUCTION_PREFIX = "${process.env.PRODUCTION_PREFIX}";
         window.nodeEnvSettings.DIGITRANSIT_API = "${process.env.DIGITRANSIT_API}";
+        window.nodeEnvSettings.MODE = "${process.env.MODE}"
     </script>
     <script>
       // WARNING: See the following for security issues around embedding JSON in HTML:
