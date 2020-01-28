1300fa6d528dd30fe655d0ce2a0c49a9a6e1f388
mikkoisa <mikko.saarinen@ceon.fi>
2020-01-28 11:20:29 +0200

    Remove unused import

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { intlShape } from 'react-intl';
 import {
-  Typography, ButtonBase, Drawer, ClickAwayListener,
+  Typography, ButtonBase, Drawer,
 } from '@material-ui/core';
 import { getIcon } from '../SMIcon';
 
