99fe6d8f48462ce8220c1001cfb40f94b1859fd4
Mikko Saarinen <mikko.saarinen@ceon.fi>
2020-02-20 08:56:02 +0200

    Remove duplicate check for connections

--- src/views/UnitView/utils/unitSectionFilter.js
+++ src/views/UnitView/utils/unitSectionFilter.js
@@ -3,8 +3,7 @@ const unitSectionFilter = (list, section) => {
   const filteredList = [];
   list.forEach((item, i) => {
     if (item.section_type === section) {
-      // Don't add duplicate elements
-      if (item.name && !filteredList.some(e => e.value.name.fi === item.name.fi)) {
+      if (item.name) {
         filteredList.push({ type: section, value: item, id: i });
       }
     }
