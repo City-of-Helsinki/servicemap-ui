b2646c17efeb24bfb4b119e4a5ce101aa293f62c
Aleksi Peiponen <aleksi.laurea@gmail.com>
2020-02-25 13:04:23 +0200

    Add default text to Loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography, LinearProgress, withStyles } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
 import styles from './styles';
 
 const Loading = (props) => {
   const { classes, text, progress } = props;
   return (
     <div className={classes.root}>
-      <Typography variant="body2" aria-hidden="true">{text}</Typography>
+      <Typography variant="body2" aria-hidden="true">{text || <FormattedMessage id="general.fetching" />}</Typography>
       <LinearProgress variant="determinate" value={Math.min(progress, 100)} />
     </div>
   );
