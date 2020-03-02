130f6b0be61475f999b36f48d7b06e4cea8eb0c6
Aleksi Peiponen <aleksi.laurea@gmail.com>
2020-03-02 13:30:09 +0200

    Prevent titlebar from getting class null

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -17,8 +17,7 @@ const TitleBar = ({
   ariaHidden,
 }) => (
   <>
-    <div className={`${className} ${classes.container} ${!backButton && !icon ? classes.textBar : ''}`}>
-
+    <div className={`${className || ''} ${classes.container} ${!backButton && !icon ? classes.textBar : ''}`}>
       {
         backButton
         && (
