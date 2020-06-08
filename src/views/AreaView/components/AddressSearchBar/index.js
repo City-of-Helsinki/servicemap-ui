cb1d5032667b8e097395687bf1b1aef9cc118bdf
Mikko Saarinen <mikko.saarinen@ceon.fi>
2020-06-08 10:56:30 +0300

    Add Swedish address results to area view search

--- src/views/AreaView/components/AddressSearchBar/index.js
+++ src/views/AreaView/components/AddressSearchBar/index.js
@@ -1,6 +1,16 @@
 import { withStyles } from '@material-ui/core';
+import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import AddressSearchBar from './AddressSearchBar';
 import styles from '../../styles';
 
-export default injectIntl(withStyles(styles)(AddressSearchBar));
+const mapStateToProps = (state) => {
+  const { locale } = state.user;
+  return {
+    locale,
+  };
+};
+
+export default injectIntl(withStyles(styles)(connect(
+  mapStateToProps,
+)(AddressSearchBar)));
