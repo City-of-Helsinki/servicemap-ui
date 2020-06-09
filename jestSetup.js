4985515c9bab8a3134b91cc87c4f0787c8b6a6a3
aleksi <aleksi.laurea@gmail.com>
2020-06-09 12:10:29 +0300

    Add SuggestionItem unit tests

--- jestSetup.js
+++ jestSetup.js
@@ -12,3 +12,18 @@ console.error = message => {
 
   originalConsoleError(message);
 };
+
+// Mock matchMedia to help with component icon props
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
