f0c6c748e1949960a9dd6258713389d97f1ad316
Mikko Saarinen <mikko.saarinen@ceon.fi>
2020-06-09 12:30:30 +0300

    Improve focus tests

--- browserTests/focusIndicatorTest.js
+++ browserTests/focusIndicatorTest.js
@@ -10,66 +10,66 @@ export default () => {
       const field = inputFields.nth(i);
       const parent = field.parent();
       const disabled = await field.getAttribute('disabled');
-  
-      const focusElement = ClientFunction(() => {
-        field().focus();
-      }, {
-       dependencies: { field }
-      });
-      
-      await focusElement()
-      
-      // Check if focus border is on the input element or its parent. (Mui InputBase puts focus on wrapper div insted of the input field itself)
-      const currentFocused = await field.getStyleProperty('box-shadow')
-      const parentFocused = await parent.getStyleProperty('box-shadow')
-      let focusedElement = null;
-
-      if (currentFocused !== 'none') {
-        focusedElement = field;
-      } else if (parentFocused !== 'none'){
-        focusedElement = parent;
-      }
 
       if (disabled === undefined || disabled === false) {
+        const focusElement = ClientFunction(() => {
+          field().focus();
+        }, {
+        dependencies: { field }
+        });
+        
+        await focusElement()
+        
+        // Check if focus border is on the input element or its parent. (Mui InputBase puts focus on wrapper div insted of the input field itself)
+        const currentFocused = await field.getStyleProperty('box-shadow')
+        const parentFocused = await parent.getStyleProperty('box-shadow')
+        let focusedElement = null;
+
+        if (currentFocused !== 'none') {
+          focusedElement = field;
+        } else if (parentFocused !== 'none'){
+          focusedElement = parent;
+        }
+
         await t
           .expect(focusedElement).ok('Input field does not have correct box-shadow focus indicator')
 
         const parentBackground = await getParentElementBG(await focusedElement, await focusedElement.parent());
-        const contrast = getContrast(parentBackground, await focusedElement.getStyleProperty('box-shadow'));
+        const contrastWithBackground = getContrast(parentBackground, await focusedElement.getStyleProperty('box-shadow'));
         const contrastWithElement = getContrast(await focusedElement.getStyleProperty('background-color'), await focusedElement.getStyleProperty('box-shadow'));
         
       
         await t
-          .expect(contrast).gte(3, `Input contrast ratio between focus border and background is not enough`)
-          .expect(contrastWithElement).gte(3, `Input contrast ratio between focus border and background is not enough`)
-     }
+          .expect(contrastWithBackground).gte(3, `Input contrast ratio between focus border and parent background is not enough`)
+          .expect(contrastWithElement).gte(3, `Input contrast ratio between focus border and element background is not enough`)
+      }
     }
   })
 
-  test('Buttons have correct focus indicator', async (t) => {
+  test('Buttons have correct focus indicators', async (t) => {
     const buttons = Selector(`main button`);
     const buttonsCount = await buttons.count;
 
     for(let i = 0; i < buttonsCount; i++) {  
       const button = buttons.nth(i);
       const disabled = await button.getAttribute('disabled');
-  
-      const focusElement = ClientFunction(() => {
-        button().focus();
-      }, {
-       dependencies: { button }
-      });
-      
-      await focusElement()
-  
-      // Focusing on Mui button element does not display focus border. Element needs to be focused with tab as well.
-      await t
-        .pressKey('tab')
-        .pressKey('shift+tab')
-        .wait(20)
-
 
       if (disabled === undefined || disabled === false) {
+        const focusElement = ClientFunction(() => {
+          button().focus();
+        }, {
+        dependencies: { button }
+        });
+        
+        await focusElement()
+    
+        // Focusing on Mui button element does not display focus border. Element needs to be focused with tab as well.
+        await t
+          .pressKey('tab')
+          .pressKey('shift+tab')
+          .wait(20)
+
+
         const focusBorder = await button.getStyleProperty('box-shadow')
         await t
           .expect(focusBorder !== 'none').ok('Input field does not have correct box-shadow focus indicator')
