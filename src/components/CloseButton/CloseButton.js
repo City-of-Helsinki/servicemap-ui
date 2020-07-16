79612f0a624d5766defef855c7f1b8a9a6585671
aleksi <aleksi.laurea@gmail.com>
2020-07-16 09:43:17 +0300

    Move settings close button to own component

---
+++ src/components/CloseButton/CloseButton.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, intlShape } from 'react-intl';
+import { Button } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+
+const CloseButton = ({
+  classes,
+  className,
+  intl,
+  onClick,
+  ...rest
+}) => (
+  <Button
+    aria-label={intl.formatMessage({ id: 'general.close' })}
+    className={`${classes.flexBase} ${classes.button} ${className || ''}`}
+    classes={{ label: classes.buttonLabel }}
+    onClick={() => {
+      onClick();
+    }}
+    {...rest}
+  >
+    <Close />
+    <FormattedMessage id="general.close" />
+  </Button>
+);
+
+CloseButton.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.any).isRequired,
+  className: PropTypes.string,
+  intl: intlShape.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+CloseButton.defaultProps = {
+  className: null,
+};
+
+export default CloseButton;
