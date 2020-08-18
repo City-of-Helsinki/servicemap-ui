8ea1f9650a0624486e6df14b3382f348893d450a
Arjeta Peshtani <arjeta.peshtani@anders.fi>
2020-08-18 08:39:23 +0300

    Turku Update: Limit the selection of the maps for the City of Turku

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -3,7 +3,7 @@ import LocalStorageUtility from './localStorage';
 const ALLOWED = {
   mobility: [null, 'wheelchair', 'reduced_mobility', 'rollator', 'stroller'],
   city: [null, 'helsinki', 'espoo', 'vantaa', 'kauniainen'],
-  map: ['servicemap', 'ortographic', 'guideMap', 'accessible_map'],
+  map: ['servicemap', 'accessible_map'],
 };
 
 const ACCESSIBILITY_MAPPING = {
