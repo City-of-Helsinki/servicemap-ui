18eb0e603b91f86f301d9b608f805d55610120c5
mikkoisa <mikko.saarinen@ceon.fi>
2021-02-02 08:59:48 +0200

    Add maintenance organisation parameters to fetch

--- src/utils/fetch/constants.js
+++ src/utils/fetch/constants.js
@@ -33,7 +33,7 @@ export const APIHandlers = {
     options: {
       page: 1,
       page_size: 200,
-      only: 'unit.street_address,unit.location,unit.name,unit.municipality,unit.accessibility_shortcoming_count,unit.contract_type',
+      only: 'unit.street_address,unit.location,unit.name,unit.municipality,unit.accessibility_shortcoming_count,unit.contract_type,unit.extensions',
       geometry: true,
     },
   },
@@ -58,7 +58,7 @@ export const APIHandlers = {
     options: {
       page: 1,
       page_size: 200,
-      only: 'street_address,location,name,municipality,accessibility_shortcoming_count,service_nodes,contract_type',
+      only: 'street_address,location,name,municipality,accessibility_shortcoming_count,service_nodes,contract_type,extensions',
       geometry: true,
       include: 'service_nodes,services,accessibility_properties',
     },
