d1907d127daea33bf4a0bdbaedba331814e2bc49
mikkoisa <mikko.saarinen@ceon.fi>
2021-02-02 08:57:13 +0200

    Add contract text helper function

--- src/utils/unitHelper.js
+++ src/utils/unitHelper.js
@@ -1,5 +1,6 @@
+/* eslint-disable camelcase */
 import { drawUnitIcon } from '../views/MapView/utils/drawIcon';
-import isClient from '.';
+import isClient, { uppercaseFirst } from '.';
 import SettingsUtility from './settings';
 import config from '../../config';
 import { isEmbed } from './path';
@@ -146,6 +147,24 @@ class UnitHelper {
     const action = paths.unit.regex.test(window.location.href)
       ? 'replace' : 'push';
     navigator[action]('unit', { id });
+  }
+
+  static getContractText = (unit, intl, getLocaleText) => {
+    const { contract_type, extensions } = unit;
+    if (!contract_type?.description) return null;
+
+    const municipalIDs = ['municipal_service', 'purchased_service'];
+    const contractText = uppercaseFirst(getLocaleText(contract_type.description));
+    const contractMunicipality = extensions.maintenance_organization;
+
+    if (contractMunicipality && municipalIDs.includes(contract_type.id)) {
+      const cityString = intl.formatMessage({ id: `settings.city.${extensions.maintenance_organization}` });
+      if (cityString) {
+        return `${contractText}, ${cityString}`;
+      }
+      return contractText;
+    }
+    return contractText;
   }
 }
 
