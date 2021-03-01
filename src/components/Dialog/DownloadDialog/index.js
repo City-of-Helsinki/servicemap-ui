9c7d00882a4a17ef7972279ae770f77e7d739fcd
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-01 10:05:39 +0200

    Add hook to DownloadDialog

--- src/components/Dialog/DownloadDialog/index.js
+++ src/components/Dialog/DownloadDialog/index.js
@@ -1,16 +1,6 @@
-import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/styles';
 import DownloadDialog from './DownloadDialog';
-import { getLocaleString } from '../../../redux/selectors/locale';
 import styles from './styles';
 
-const mapStateToProps = (state) => {
-  const getLocaleText = textObject => getLocaleString(state, textObject);
 
-  return {
-    getLocaleText,
-  };
-};
-
-
-export default withStyles(styles)(connect(mapStateToProps)(DownloadDialog));
+export default withStyles(styles)(DownloadDialog);
