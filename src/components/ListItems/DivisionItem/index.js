dbfd0f1e8711f70d0cf6ba3296280c708a39d9f4
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-01 10:36:54 +0200

    Add hook to DivisionItem

--- src/components/ListItems/DivisionItem/index.js
+++ src/components/ListItems/DivisionItem/index.js
@@ -3,15 +3,12 @@ import { withStyles } from '@material-ui/core';
 import { injectIntl } from 'react-intl';
 import styles from './styles';
 import DivisionItem from './DivisionItem';
-import { getLocaleString } from '../../../redux/selectors/locale';
 
 const mapStateToProps = (state) => {
-  const getLocaleText = textObject => getLocaleString(state, textObject);
   const { navigator, user } = state;
   const { locale } = user;
 
   return {
-    getLocaleText,
     locale,
     navigator,
   };
