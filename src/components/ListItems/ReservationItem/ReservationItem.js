3f14c324df7071fc005b16b558f317232cfab4c9
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-01 10:41:06 +0200

    Add hook to ReservationItem

--- src/components/ListItems/ReservationItem/ReservationItem.js
+++ src/components/ListItems/ReservationItem/ReservationItem.js
@@ -2,22 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { EventAvailable } from '@material-ui/icons';
 import SimpleListItem from '../SimpleListItem';
+import useLocaleText from '../../../utils/useLocaleText';
 
-const ReservationItem = ({ getLocaleText, reservation, intl }) => (
-  <SimpleListItem
-    key={reservation.id}
-    icon={<EventAvailable />}
-    link
-    text={`${getLocaleText(reservation.name)} ${intl.formatMessage({ id: 'unit.opens.new.tab' })}`}
-    divider
-    handleItemClick={() => {
-      window.open(`https://varaamo.hel.fi/resources/${reservation.id}`);
-    }}
-  />
-);
+const ReservationItem = ({ reservation, intl }) => {
+  const getLocaleText = useLocaleText();
+  return (
+    <SimpleListItem
+      key={reservation.id}
+      icon={<EventAvailable />}
+      link
+      text={`${getLocaleText(reservation.name)} ${intl.formatMessage({ id: 'unit.opens.new.tab' })}`}
+      divider
+      handleItemClick={() => {
+        window.open(`https://varaamo.hel.fi/resources/${reservation.id}`);
+      }}
+    />
+  );
+};
 
 ReservationItem.propTypes = {
-  getLocaleText: PropTypes.func.isRequired,
   intl: PropTypes.objectOf(PropTypes.any).isRequired,
   reservation: PropTypes.shape({
     id: PropTypes.string,
