d49fc4574b5f3c0e0536e4a498ac9e0bb4673d25
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-01 10:34:10 +0200

    Add hook to FeedbackView

--- src/views/FeedbackView/index.js
+++ src/views/FeedbackView/index.js
@@ -3,17 +3,14 @@ import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core';
 import { injectIntl } from 'react-intl';
 import FeedbackView from './FeedbackView';
-import { getLocaleString } from '../../redux/selectors/locale';
 import styles from './styles';
 
 const mapStateToProps = (state) => {
   const { navigator } = state;
   const selectedUnit = state.selectedUnit.unit.data;
-  const getLocaleText = textObject => getLocaleString(state, textObject);
   return {
     navigator,
     selectedUnit,
-    getLocaleText,
   };
 };
 
