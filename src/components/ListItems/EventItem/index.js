37eb9dc44a6bf3a0da66dca61fb1f1adbcdf6884
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-01 10:39:11 +0200

    Add hook to EventItem

--- src/components/ListItems/EventItem/index.js
+++ src/components/ListItems/EventItem/index.js
@@ -2,17 +2,14 @@
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import EventItem from './EventItem';
-import { getLocaleString } from '../../../redux/selectors/locale';
 import { changeSelectedEvent } from '../../../redux/actions/event';
 
 // Listen to redux state
 const mapStateToProps = (state) => {
-  const getLocaleText = textObject => getLocaleString(state, textObject);
   const {
     navigator,
   } = state;
   return {
-    getLocaleText,
     navigator,
   };
 };
