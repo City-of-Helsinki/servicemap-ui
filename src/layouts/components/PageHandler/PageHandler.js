6656fa6083f07b673ce1f2188e80d518a48e8451
mikkoisa <mikko.saarinen@ceon.fi>
2021-03-29 10:01:08 +0300

    Refactor PageHandler to functional component

--- src/layouts/components/PageHandler/PageHandler.js
+++ src/layouts/components/PageHandler/PageHandler.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { uppercaseFirst } from '../../../utils';
+import useLocaleText from '../../../utils/useLocaleText';
 
-class PageHandler extends React.Component {
-  componentDidMount() {
-    const { page, setCurrentPage } = this.props;
+const PageHandler = (props) => {
+  const {
+    page, setCurrentPage, intl, messageId, unit, service, event,
+  } = props;
+
+  const getLocaleText = useLocaleText();
+
+  useEffect(() => {
     // Save current page to redux
     setCurrentPage(page);
-  }
+  }, []);
 
   // Modify html head
-  render() {
-    const {
-      intl, messageId, page, unit, service, event, getLocaleText,
-    } = this.props;
-    const message = messageId ? intl.formatMessage({ id: messageId }) : '';
-    let pageMessage = '';
+  const message = messageId ? intl.formatMessage({ id: messageId }) : '';
+  let pageMessage = '';
 
-    // Add unit or service name to title if needed
-    if ((page === 'unit' || page === 'eventList') && unit && unit.name) {
-      pageMessage = getLocaleText(unit.name);
-    } if (page === 'service' && service && service.name) {
-      pageMessage = getLocaleText(service.name);
-    } if (page === 'event' && event && event.name) {
-      pageMessage = getLocaleText(event.name);
-    }
-
-    let appTitle = intl.formatMessage({ id: 'app.title' });
-
-    if (message !== '' || pageMessage !== '') {
-      appTitle = ` | ${appTitle}`;
-    }
-
-    const title = `${uppercaseFirst(pageMessage)} ${message}${appTitle}`;
-
-    return (
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
-    );
+  // Add unit or service name to title if needed
+  if ((page === 'unit' || page === 'eventList') && unit && unit.name) {
+    pageMessage = getLocaleText(unit.name);
+  } if (page === 'service' && service && service.name) {
+    pageMessage = getLocaleText(service.name);
+  } if (page === 'event' && event && event.name) {
+    pageMessage = getLocaleText(event.name);
   }
-}
+
+  let appTitle = intl.formatMessage({ id: 'app.title' });
+
+  if (message !== '' || pageMessage !== '') {
+    appTitle = ` | ${appTitle}`;
+  }
+
+  const title = `${uppercaseFirst(pageMessage)} ${message}${appTitle}`;
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+    </Helmet>
+  );
+};
+
 
 PageHandler.propTypes = {
   intl: PropTypes.objectOf(PropTypes.any).isRequired,
@@ -50,7 +52,6 @@ PageHandler.propTypes = {
   unit: PropTypes.objectOf(PropTypes.any),
   event: PropTypes.objectOf(PropTypes.any),
   service: PropTypes.objectOf(PropTypes.any),
-  getLocaleText: PropTypes.func.isRequired,
   setCurrentPage: PropTypes.func.isRequired,
 };
 
--- src/layouts/components/PageHandler/index.js
+++ src/layouts/components/PageHandler/index.js
@@ -1,18 +1,14 @@
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import { setCurrentPage } from '../../../redux/actions/user';
-import { getLocaleString } from '../../../redux/selectors/locale';
 import PageHandler from './PageHandler';
 
 const mapStateToProps = (state) => {
   const { selectedUnit, service, event } = state;
-  // TODO: replace this with useLocaleText when the component is converted to function component
-  const getLocaleText = textObject => getLocaleString(state, textObject);
   return {
     unit: selectedUnit.unit.data,
     service: service.current,
     event,
-    getLocaleText,
   };
 };
 
