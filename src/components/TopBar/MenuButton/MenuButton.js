ba03a56787e649784a15210a5db386af938b9d02
mikkoisa <mikko.saarinen@ceon.fi>
2021-05-04 13:32:18 +0300

    Use intl hook

--- src/components/TopBar/MenuButton/MenuButton.js
+++ src/components/TopBar/MenuButton/MenuButton.js
@@ -2,42 +2,45 @@ import { Button, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { Close, Menu } from '@material-ui/icons';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 
 const MenuButton = ({
-  classes, intl, drawerOpen, pageType, toggleDrawerMenu,
-}) => (
-  <Button
-    id="MenuButton"
-    aria-label={intl.formatMessage({ id: 'general.menu' })}
-    aria-expanded={drawerOpen}
-    aria-haspopup="true"
-    className={`${drawerOpen ? classes.toolbarButtonPressed : classes.toolbarButton} ${pageType !== 'mobile' ? classes.largeButton : ''}`}
-    classes={{ label: classes.buttonLabel }}
-    onClick={() => toggleDrawerMenu()}
-  >
-    {drawerOpen ? (
-      <>
-        <Close />
-        <Typography color="inherit" variant="body2">
-          <FormattedMessage id="general.close" />
-        </Typography>
-      </>
-    ) : (
-      <>
-        <Menu />
-        <Typography color="inherit" variant="body2">
-          <FormattedMessage id="general.menu" />
-        </Typography>
-      </>
-    )}
-  </Button>
-);
+  classes, drawerOpen, pageType, toggleDrawerMenu,
+}) => {
+  const intl = useIntl();
+
+  return (
+    <Button
+      id="MenuButton"
+      aria-label={intl.formatMessage({ id: 'general.menu' })}
+      aria-expanded={drawerOpen}
+      aria-haspopup="true"
+      className={`${drawerOpen ? classes.toolbarButtonPressed : classes.toolbarButton} ${pageType !== 'mobile' ? classes.largeButton : ''}`}
+      classes={{ label: classes.buttonLabel }}
+      onClick={() => toggleDrawerMenu()}
+    >
+      {drawerOpen ? (
+        <>
+          <Close />
+          <Typography color="inherit" variant="body2">
+            <FormattedMessage id="general.close" />
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Menu />
+          <Typography color="inherit" variant="body2">
+            <FormattedMessage id="general.menu" />
+          </Typography>
+        </>
+      )}
+    </Button>
+  );
+};
 
 MenuButton.propTypes = {
   classes: PropTypes.objectOf(PropTypes.any).isRequired,
-  intl: PropTypes.objectOf(PropTypes.any).isRequired,
   drawerOpen: PropTypes.bool.isRequired,
   pageType: PropTypes.string,
   toggleDrawerMenu: PropTypes.func.isRequired,
--- src/components/TopBar/MenuButton/index.js
+++ src/components/TopBar/MenuButton/index.js
@@ -1,6 +1,5 @@
 import { withStyles } from '@material-ui/core';
-import { injectIntl } from 'react-intl';
 import MenuButton from './MenuButton';
 import styles from '../styles';
 
-export default injectIntl(withStyles(styles)(MenuButton));
+export default withStyles(styles)(MenuButton);
