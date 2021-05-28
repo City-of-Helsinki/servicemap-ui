d3120e137f8e8c53b4e11f6419683fe03d342545
mikkoisa <mikko.saarinen@ceon.fi>
2021-05-28 09:59:48 +0300

    Fix server mismatch error

--- src/components/DescriptionText/DescriptionText.js
+++ src/components/DescriptionText/DescriptionText.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Divider } from '@material-ui/core';
-import isClient from '../../utils';
+import { Typography, Divider, NoSsr } from '@material-ui/core';
 
 const DescriptionText = ({
   description, html, classes, title, titleComponent,
@@ -9,28 +8,27 @@ const DescriptionText = ({
   // Hide linebreak html elements from screen readers
   const hideBRFromSR = text => text.replaceAll('<br>', '<br aria-hidden="true" />');
 
-  // Rendering only in client since dangerouslySetInnerHTML causes mismatch errors
-  // between server and client HTML and not rendering anything on client side
-  // TODO: Figure out a way to have server render description text identical to client
-  if (description && isClient()) {
+  if (description) {
     return (
-      <div className={classes.left}>
-        <Typography
-          className={classes.subtitle}
-          component={titleComponent}
-          variant="subtitle1"
-        >
-          {title}
-        </Typography>
-        <Divider className={classes.divider} aria-hidden="true" />
-        { !html ? (
-          <Typography className={classes.paragraph} variant="body2">
-            {description}
+      <NoSsr>
+        <div className={classes.left}>
+          <Typography
+            className={classes.subtitle}
+            component={titleComponent}
+            variant="subtitle1"
+          >
+            {title}
           </Typography>
-        ) : (
-          <Typography dangerouslySetInnerHTML={{ __html: hideBRFromSR(description) }} className={classes.paragraph} variant="body2" />
-        )}
-      </div>
+          <Divider className={classes.divider} aria-hidden="true" />
+          { !html ? (
+            <Typography className={classes.paragraph} variant="body2">
+              {description}
+            </Typography>
+          ) : (
+            <Typography dangerouslySetInnerHTML={{ __html: hideBRFromSR(description) }} className={classes.paragraph} variant="body2" />
+          )}
+        </div>
+      </NoSsr>
     );
   } return null;
 };
