c5e277f5445fec3ddec438522ddd1de6a2c9ab27
mikkoisa <mikko.saarinen@ceon.fi>
2021-06-09 12:31:58 +0300

    Revert changes to event description

--- src/components/DescriptionText/DescriptionText.js
+++ src/components/DescriptionText/DescriptionText.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Divider, NoSsr } from '@material-ui/core';
+import { Typography, Divider } from '@material-ui/core';
+import isClient from '../../utils';
 
 const DescriptionText = ({
   description, html, classes, title, titleComponent,
@@ -8,27 +9,25 @@ const DescriptionText = ({
   // Hide linebreak html elements from screen readers
   const hideBRFromSR = text => text.replaceAll('<br>', '<br aria-hidden="true" />');
 
-  if (description) {
+  if (description && isClient()) {
     return (
-      <NoSsr>
-        <div className={classes.left}>
-          <Typography
-            className={classes.subtitle}
-            component={titleComponent}
-            variant="subtitle1"
-          >
-            {title}
+      <div className={classes.left}>
+        <Typography
+          className={classes.subtitle}
+          component={titleComponent}
+          variant="subtitle1"
+        >
+          {title}
+        </Typography>
+        <Divider className={classes.divider} aria-hidden="true" />
+        { !html ? (
+          <Typography className={classes.paragraph} variant="body2">
+            {description}
           </Typography>
-          <Divider className={classes.divider} aria-hidden="true" />
-          { !html ? (
-            <Typography className={classes.paragraph} variant="body2">
-              {description}
-            </Typography>
-          ) : (
-            <Typography dangerouslySetInnerHTML={{ __html: hideBRFromSR(description) }} className={classes.paragraph} variant="body2" />
-          )}
-        </div>
-      </NoSsr>
+        ) : (
+          <Typography dangerouslySetInnerHTML={{ __html: hideBRFromSR(description) }} className={classes.paragraph} variant="body2" />
+        )}
+      </div>
     );
   } return null;
 };
