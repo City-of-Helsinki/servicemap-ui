b3e4a1af2af0c24fc538ecdfd7a3356b4319fc0e
aleksi <aleksi.laurea@gmail.com>
2021-06-16 10:30:05 +0300

    Change DownloadDialog service name fetcing to use new fetch functionality

--- src/components/Dialog/DownloadDialog/utils.js
+++ src/components/Dialog/DownloadDialog/utils.js
@@ -1,19 +1,17 @@
 import config from '../../../../config';
+import ServiceMapAPI from '../../../utils/newFetch/ServiceMapAPI';
 
-// Fetch service names using givcen service_node id's
-export const fetchServiceNames = (ids) => {
+const smAPI = new ServiceMapAPI();
+
+export const fetchServiceNames = async (ids) => {
   if (!ids) {
     return null;
   }
 
-  return fetch(`${config.serviceMapAPI.root}/service_node/?id=${ids}&page=1&page_size=1000`)
-    .then(response => response.json())
+  return await smAPI.serviceNames(ids)
     .then((data) => {
-      const nameArray = data?.results?.map(v => v.name);
-      return nameArray;
-    }).catch((e) => {
-      console.error('Error while fetching service names', e);
-    });
-};
+      return data?.map(v => v.name);
+    })
+}
 
 export default fetchServiceNames;
