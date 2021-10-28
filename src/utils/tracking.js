d0713a6828ce8ff19b0eff3f752f5019f21ae669
mikkoisa <mikko.saarinen@ceon.fi>
2021-10-28 07:44:35 +0300

    Disable matomo tracking if no cookies allowed

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -1,13 +1,13 @@
 import MatomoTracker from '@datapunt/matomo-tracker-js';
 import config from '../../config';
 
-const matomoTracker = (config.matomoSiteId && config.matomoUrl)
+const matomoTracker = (config.matomoSiteId && config.matomoUrl && window?.cookiehub?.hasConsented('analytics'))
   ? new MatomoTracker({
     urlBase: `https://${config.matomoUrl}`,
     siteId: config.matomoSiteId,
     trackerUrl: `https://${config.matomoUrl}/tracker.php`, // optional, default value: `${urlBase}matomo.php`
     srcUrl: `https://${config.matomoUrl}/piwik.min.js`, // optional, default value: `${urlBase}matomo.js`
-    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    disabled: !window.cookiehub.hasConsented('analytics'), // optional, false by default. Makes all tracking calls no-ops if set to true.
     linkTracking: false, // optional, default value: true
   }) : null;
 
