4f178a05538e55c7c7f28857c043d21779af5791
mikkoisa <mikko.saarinen@ceon.fi>
2021-11-02 14:14:50 +0200

    Add better window null check

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -1,13 +1,17 @@
 import MatomoTracker from '@datapunt/matomo-tracker-js';
 import config from '../../config';
 
-const matomoTracker = (config.matomoSiteId && config.matomoUrl && window?.cookiehub?.hasConsented('analytics'))
+const matomoTracker = (
+  typeof window !== 'undefined'
+  && config.matomoSiteId
+  && config.matomoUrl
+  && window?.cookiehub?.hasConsented('analytics'))
   ? new MatomoTracker({
     urlBase: `https://${config.matomoUrl}`,
     siteId: config.matomoSiteId,
     trackerUrl: `https://${config.matomoUrl}/tracker.php`, // optional, default value: `${urlBase}matomo.php`
     srcUrl: `https://${config.matomoUrl}/piwik.min.js`, // optional, default value: `${urlBase}matomo.js`
-    disabled: !window.cookiehub.hasConsented('analytics'), // optional, false by default. Makes all tracking calls no-ops if set to true.
+    disabled: !window?.cookiehub?.hasConsented('analytics'), // optional, false by default. Makes all tracking calls no-ops if set to true.
     linkTracking: false, // optional, default value: true
   }) : null;
 
