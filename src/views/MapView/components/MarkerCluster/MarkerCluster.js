9ab6092a527a919c4e2a2668ba6b13e3f4e3ca7d
mikkoisa <mikko.saarinen@ceon.fi>
2022-05-17 13:44:45 +0300

    Move marker popup to view on mobile

--- src/views/MapView/components/MarkerCluster/MarkerCluster.js
+++ src/views/MapView/components/MarkerCluster/MarkerCluster.js
@@ -21,9 +21,9 @@ const tooltipOptions = (permanent, classes) => ({
   offset: [0, -25],
 });
 
-const popupOptions = () => ({
+const popupOptions = isMobile => ({
   autoClose: false,
-  autoPan: false,
+  autoPan: !!isMobile,
   closeButton: true,
   closeOnClick: false,
   direction: 'top',
@@ -389,7 +389,7 @@ const MarkerCluster = ({
           },
         ).bindPopup(
           popupContent,
-          popupOptions(),
+          popupOptions(isMobile),
         );
 
         if (isMobile) {
