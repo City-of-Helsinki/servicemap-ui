c47346735da312b7610a2c0c62a463c981f3c763
mikkoisa <mikko.saarinen@ceon.fi>
2022-05-17 07:50:14 +0300

    Use city settings on service suggestions

--- src/components/SearchBar/createSuggestions.js
+++ src/components/SearchBar/createSuggestions.js
@@ -9,7 +9,8 @@ const createSuggestions = (query, abortController, getLocaleText, citySettings) 
   const unitLimit = 10;
   const serviceLimit = 10;
   const addressLimit = 1;
-  const pageSize = unitLimit + serviceLimit + addressLimit;
+  const servicenodeLimit = 10;
+  const pageSize = unitLimit + serviceLimit + addressLimit + servicenodeLimit;
 
   const additionalOptions = {
     page_size: pageSize,
@@ -17,13 +18,33 @@ const createSuggestions = (query, abortController, getLocaleText, citySettings) 
     unit_limit: unitLimit,
     service_limit: serviceLimit,
     address_limit: addressLimit,
+    servicenode_limit: servicenodeLimit,
     municipality: citySettings.join(','),
   };
 
   const results = await smAPI.search(query, additionalOptions);
 
+  let filteredResults = results;
+
+  // Filter services with city settings
+  if (citySettings.length) {
+    filteredResults = filteredResults.filter((result) => {
+      if (result.object_type === 'service' || result.object_type === 'servicenode') {
+        let totalResultCount = 0;
+        citySettings.forEach((city) => {
+          if (result.unit_count?.municipality[city]) {
+            totalResultCount += result.unit_count.municipality[city];
+          }
+        });
+        if (totalResultCount === 0) return false;
+      }
+      return true;
+    });
+  }
+
+
   // Handle address results
-  results.forEach((item) => {
+  filteredResults.forEach((item) => {
     if (item.object_type === 'address') {
       if (getLocaleText(item.name).toLowerCase() === query.toLowerCase()) {
         item.isExact = true;
@@ -31,7 +52,7 @@ const createSuggestions = (query, abortController, getLocaleText, citySettings) 
     }
   });
 
-  return results;
+  return filteredResults;
 };
 
 export default createSuggestions;
