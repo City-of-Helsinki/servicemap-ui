b62a9331bdf22a2c3ee20be61ba4a03387a70131
AleksiP <aleksi.laurea@gmail.com>
2022-06-22 13:17:20 +0300

    Move Language menu to own component and add unit tests

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -6,23 +6,21 @@ import {
 import { visuallyHidden } from '@mui/utils';
 import { Map } from '@mui/icons-material';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useLocation } from 'react-router-dom';
 import DrawerMenu from './DrawerMenu';
 import DesktopComponent from '../DesktopComponent';
 import MobileComponent from '../MobileComponent';
 import ToolMenu from '../ToolMenu';
 import { focusToViewTitle } from '../../utils/accessibility';
-import LocaleUtility from '../../utils/locale';
 import { useNavigationParams } from '../../utils/address';
 import SettingsButton from './SettingsButton';
 import MenuButton from './MenuButton';
 import SMLogo from './SMLogo';
 import { isHomePage } from '../../utils/path';
+import LanguageMenu from './LanguageMenu';
 
 const TopBar = (props) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const locale = useSelector(state => state.user.locale);
   const location = useLocation();
   const intl = useIntl();
   const getAddressNavigatorParams = useNavigationParams();
@@ -114,42 +112,6 @@ const TopBar = (props) => {
     />
   );
 
-  const renderLanguages = (pageType) => {
-    const typographyClass = className => `${pageType === 'mobile' ? classes.mobileFont : ''} ${className || ''}`;
-    return (
-      <>
-        {LocaleUtility.availableLocales
-          .map(currentLocale => (
-            <ButtonBase
-              aria-current={currentLocale === locale ? 'true' : false}
-              role="link"
-              key={currentLocale}
-              focusVisibleClassName={classes.topButtonFocused}
-              lang={currentLocale}
-              onClick={() => {
-                const newLocation = location;
-                const newPath = location.pathname.replace(/^\/[a-zA-Z]{2}/, `/${currentLocale}`);
-                newLocation.pathname = newPath;
-                window.location = `${newLocation.pathname}${newLocation.search}`;
-              }}
-            >
-              <Typography
-                className={typographyClass(
-                  currentLocale === locale
-                    ? classes.bold
-                    : classes.greyText,
-                )}
-                color="inherit"
-                variant="body2"
-              >
-                <FormattedMessage id={`general.language.${currentLocale}`} />
-              </Typography>
-            </ButtonBase>
-          ))}
-      </>
-    );
-  };
-
   const handleContrastChange = () => {
     changeTheme(theme === 'default' ? 'dark' : 'default');
     setMapType(theme === 'default' ? 'accessible_map' : 'servicemap');
@@ -232,7 +194,7 @@ const TopBar = (props) => {
                 </Typography>
               </ButtonBase>
               <Typography aria-hidden color="inherit">|</Typography>
-              {renderLanguages(pageType)}
+              <LanguageMenu mobile={pageType === 'mobile'} />
               <Typography aria-hidden color="inherit">|</Typography>
               <ButtonBase role="button" onClick={() => handleContrastChange()} focusVisibleClassName={classes.topButtonFocused} aria-label={contrastAriaLabel}>
                 <Typography className={fontClass} color="inherit" variant="body2"><FormattedMessage id="general.contrast" /></Typography>
