853901e609fc87738eb7e1a4436f048196dba90d
Aleksi Peiponen <peiponen.aleksi@gmail.com>
2022-08-12 09:52:45 +0300

    Add new browser tests for address view

--- browserTests/views/addressTest.js
+++ browserTests/views/addressTest.js
@@ -3,9 +3,13 @@ import { Selector } from 'testcafe';
 import { waitForReact, ReactSelector } from 'testcafe-react-selectors';
 
 import config from '../config';
+import { getLocation } from '../utility';
+
 const { server } = config;
+const testLocation = `http://${server.address}:${server.port}/fi/address/helsinki/Topeliuksenkatu/27`;
+
 fixture`Address view test`
-  .page`http://${server.address}:${server.port}/fi/address/helsinki/Topeliuksenkatu/27`
+  .page`${testLocation}`
   .beforeEach(async () => {
     await waitForReact();
   });
@@ -60,3 +64,90 @@ test('AddressView map renders correctly', async (t) => {
     .expect(markers).gt(1)
   ;
 });
+
+test('AddressView\'s area view link does take correct address to AreaView', async (t) => {
+  const areaViewLink = Selector('#areaViewLink');
+  const addressBar = Selector('#addressSearchbar')
+  const addressMarker = Selector('div[class*="leaflet-marker-icon"][class*="addressIcon"]')
+
+  await t
+    .click(areaViewLink)
+    .expect(getLocation()).contains('/fi/area')
+    .expect(addressBar.value).contains('Topeliuksenkatu 27, Helsinki')
+    .expect(addressMarker.exists).ok('Address marker should be shown on map')
+  ;
+});
+
+test('AddressView buttons work correctly', async (t) => {
+  const buttons = Selector('#tab-content-0 ul button[role="link"]');
+  const buttonTitleText = await buttons.nth(1).find('p[aria-hidden="true"]').textContent;
+  const unitTitle = Selector('.TitleText');
+  const backToAddressButton = Selector('#SearchBar .SMBackButton')
+
+  await t
+    .expect(buttons.count).gt(0, 'Service buttons should exist for AddressView')
+  ;
+
+  for(let i = 0; i < buttons.count; i++) {
+    await t
+      .click(buttons.nth(1))
+      .expect(unitTitle.textContent).eql(buttonTitleText, 'Unit should have same title text as button')
+      .click(backToAddressButton)
+    ;
+  }
+});
+
+test('AddressView health station links work correctly', async (t) => {
+  const links = Selector('#tab-content-0 a');
+  const unitTitle = Selector('.TitleText');
+
+  await t
+    .click(links.nth(0))
+    .expect(getLocation()).contains('/fi/unit/62976')
+    .expect(unitTitle.textContent).contains('Uusi lastensairaala')
+    .navigateTo(testLocation)
+    .click(links.nth(1))
+    .expect(getLocation()).eql('https://www.hus.fi/potilaalle/sairaalat-ja-toimipisteet/uusi-lastensairaala')
+    .navigateTo(testLocation)
+    // .click(links.nth(2))
+    // .expect(getLocation()).contains('/fi/unit/26104')
+    // .expect(unitTitle.textContent).contains('Haarmanin sairaala')
+    // .navigateTo(testLocation)
+    .click(links.nth(2))
+    .expect(getLocation()).eql('https://www.hel.fi/helsinki/fi/sosiaali-ja-terveyspalvelut/terveyspalvelut/paivystys')
+    .navigateTo(testLocation)
+  ;
+});
+
+test('AddressView nearby services tab works correctly', async (t) => {
+  const tabNearbyUnits = Selector('button[role="tab"]').nth(1);
+  const unitTitle = Selector('.TitleText');
+  const listItems = Selector('#tab-content-1 li[role="link"]');
+  const listItemTopRow = (listItem) => listItem.find('div[class*="ResultItem-topRow"] p[aria-hidden="true"]');
+  const backToAddressButton = Selector('#SearchBar .SMBackButton')
+
+  await t
+    .click(tabNearbyUnits)
+  ;
+
+  const clickedItem = listItems.nth(5);
+  const clickedItemText = await listItemTopRow(clickedItem).textContent;
+  await t
+    .click(clickedItem)
+    .expect(unitTitle.textContent).eql(clickedItemText)
+    .click(backToAddressButton)
+  ;
+
+  // Test pagination
+  const firstUnitText = await listItemTopRow(listItems.nth(0)).textContent;
+  const pagination = ReactSelector('PaginationComponent');
+  const buttons = pagination.find('button');
+  await t
+  .expect(listItemTopRow(listItems.nth(0)).textContent).eql(firstUnitText, 'List items should change on pagination page change')
+    .click(buttons.nth(1))
+    .expect(getLocation()).contains('p=2')
+    .expect(listItemTopRow(listItems.nth(0)).textContent).notEql(firstUnitText, 'List items should change on pagination page change')
+  ;
+
+});
+
