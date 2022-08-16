63e3efcc95a6f75f2575e636d60d88432893959a
mikkoisa <mikko.saarinen@ceon.fi>
2022-08-16 14:31:26 +0300

    Use same address page url for different laguages

--- src/utils/address.js
+++ src/utils/address.js
@@ -33,17 +33,14 @@ export const addressMatchParamsToFetchOptions = (match) => {
   return data;
 };
 
-export const getAddressNavigatorParamsConnector = (getLocaleText, address) => {
-  if (typeof getLocaleText !== 'function') {
-    throw Error('getAddressNavigatorParams requires getLocaleText function');
-  }
+export const getAddressNavigatorParamsConnector = (address) => {
   const nameObject = address.name || address.full_name;
   // TODO: why address endpoint returns different municipality object than search endpoint?
   const municipality = address.municipality.id || address.municipality.name.fi;
 
   return {
-    municipality,
-    name: getLocaleText(nameObject),
+    municipality: municipality.toLowerCase(),
+    name: nameObject.fi, // Use Finnish value to have same url for all languages
   };
 };
 
@@ -83,10 +80,7 @@ export const getAddressFromUnit = (unit, getLocaleText, intl) => {
   return `${typeof unit.street_address === 'string' ? unit.street_address : getLocaleText(unit.street_address)}${postalCode} ${city}`;
 };
 
-export const useNavigationParams = () => {
-  const getLocaleText = useLocaleText();
-  return useCallback(
-    address => getAddressNavigatorParamsConnector(getLocaleText, address),
-    [getLocaleText],
-  );
-};
+export const useNavigationParams = () => useCallback(
+  address => getAddressNavigatorParamsConnector(address),
+  [],
+);
