3b7294e0df8994d8efdd8d3a50a8dd9df9856e1e
Aleksi Peiponen <peiponen.aleksi@gmail.com>
2022-09-01 11:12:06 +0300

    Update swedish translations

--- src/i18n/sv.js
+++ src/i18n/sv.js
@@ -456,7 +456,7 @@ const translations = {
     one {# tjänst}
     other {# tjänster}
   }`,
-  'unit.subgroup.title': 'See group specific contanct information', // TODO: Translate
+  'unit.subgroup.title': 'Se gruppspecifika kontaktuppgifter',
   'unit.educationServices': 'Verksamhetsställets tjänster per läsår',
   'unit.educationServices.description': 'Läsåret {period}',
   'unit.educationServices.more': 'Visa fler tjänster ({count})',
