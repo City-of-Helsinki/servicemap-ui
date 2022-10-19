207ae32c1bb06638e3dd795c80c5985583c6dfae
Aleksi Peiponen <peiponen.aleksi@gmail.com>
2022-10-19 15:43:51 +0300

    Add statistical area tests for navigation

--- browserTests/views/areaTest.js
+++ browserTests/views/areaTest.js
@@ -16,6 +16,28 @@ const drawerButtons = ReactSelector('ServiceTab SMAccordion');
 const radioButtons = ReactSelector('DistrictToggleButton');
 const accordions = ReactSelector('SMAccordion');
 
+// Get inner accordions for given element
+// Expects ReactSelector element as second parameter
+const openInnerAccordion = async (t, element) => {
+  const clickedAccordion = element.child().findReact('SMAccordion').nth(0);
+  await t
+    .click(clickedAccordion)
+  ;
+  return clickedAccordion;
+}
+
+// Open statistical totals section in area view
+const openStatisticalTotals = async (t) => {
+  const statisticalAccordion = accordions.nth(2);
+  await t
+    .click(statisticalAccordion)
+  ;
+  const ageAccordion = await openInnerAccordion(t, statisticalAccordion);
+  const totalAccordion = await openInnerAccordion(t, ageAccordion);
+
+  return totalAccordion;
+}
+
 test('District lists are fetched and rendered correctly', async (t) => {
   const rootAccordionLength = await accordions.count;
 
@@ -100,5 +122,75 @@ test('Embeder tool does not crash area view', async (t) => {
     .click(toolMenu.child(0))
     .click(closeEmbedderButton)
     .expect(toolMenuButton.exists).ok('Area view was not rendered correctly')
+});
+
+test('Statistical areas accordions open correctly', async (t) => {
+  const totalAccordion = await openStatisticalTotals(t);
+
+  const innerAccordions = totalAccordion.child().findReact('SMAccordion');
+  const cityAccordions = totalAccordion.child().find('#StatisticalCityList').findReact('SMAccordion');
+  const serviceAccordion = await innerAccordions.nth(0);
+  const helsinkiAccordion = await cityAccordions.nth(0);
+  await t
+    .expect(cityAccordions.count).gt(0, 'Statistical section should show city accordions')
+    .expect(serviceAccordion.find('button').getAttribute('disabled')).eql('', 'Expect service button disabled attribute to exist')
+    .click(helsinkiAccordion.find('input[type="checkbox"]').nth(0))
+    .expect(serviceAccordion.find('button').getAttribute('disabled')).notOk()
+    .click(serviceAccordion)
+  ;
+
+  const serviceListAccordions = serviceAccordion.child().findReact('SMAccordion');
+  await t
+    .expect(serviceListAccordions.count).gt(0, 'Service list should have accordion elements', { timeout: 8000 })
+    .click(serviceListAccordions.nth(0).find('input[type="checkbox"]'))
+    .click(serviceListAccordions.nth(0).find('button'))
+  ;
+  
+  const unitItems = serviceListAccordions.nth(0).child().findReact('ResultItem');
+  await t
+    .expect(unitItems.count).gt(0)
+  ;
+})
+
+test('Statistical area district selection works correctly', async (t) => {
+  const totalAccordion = await openStatisticalTotals(t);
+
+  const serviceButton = await totalAccordion.child().findReact('SMAccordion').nth(0).find('button');
+  // const innerAccordions = totalAccordion.child().findReact('SMAccordion');
+  const cityAccordions = totalAccordion.child().find('#StatisticalCityList').findReact('SMAccordion');
+  const firstCityAreaCheckbox = cityAccordions.nth(0).find('div').nth(1).find('.MuiCollapse-root input[type="checkbox"]');
+  const firstCityCheckbox = cityAccordions.nth(0).find('input[type="checkbox"]');
+
+  await t
+    .click(cityAccordions.nth(0))
+    .click(firstCityAreaCheckbox.nth(0))
+    .expect(serviceButton.getAttribute('disabled')).notOk('Expect service button to be active while selections exist')
+    // Test indeterminate functionality
+    .expect(firstCityCheckbox.getAttribute('data-indeterminate')).eql('true')
+    .click(firstCityCheckbox)
+    .expect(firstCityCheckbox.getAttribute('data-indeterminate')).eql('false')
+    .click(firstCityCheckbox) // Remove selection
+    .expect(serviceButton.getAttribute('disabled')).eql('', 'Expect service button disabled attribute to exist while no selections')
+
+    // Test keyboard functionality
+    .pressKey('shift+tab')
+    .pressKey('tab')
+    .expect(firstCityCheckbox.focused).ok('City checkbox should be focused')
+    .pressKey('tab') // Go to city button
+    .pressKey('tab') // Go to first district checkbox
+    .expect(firstCityCheckbox.focused).notOk('City should not be focused anymore')
+    .expect(firstCityAreaCheckbox.focused).ok() // First area checkbox should be focused
+    .expect(firstCityCheckbox.getAttribute('data-indeterminate')).eql('false') // City checkbox shouldn't be indeterminate
+    .pressKey('space') // Select first district in first city
+    .expect(serviceButton.getAttribute('disabled')).notOk('Expect service button to be active while selections exist')
+    .expect(firstCityCheckbox.getAttribute('data-indeterminate')).eql('true') // City checkbox should be indeterminate
+    .pressKey('shift+tab') // Return to city button
+    .pressKey('shift+tab') // Return to city checkbox
+    .expect(firstCityCheckbox.focused).ok() // City checkbox should be focused
+    .pressKey('enter') // Select city
+    .pressKey('enter') // Remove selection
+    .expect(serviceButton.getAttribute('disabled')).eql('', 'Expect service button disabled attribute to exist while no selections')
+
+  ;
 });
 
