14a361f01affeae529a4c1aa6fb16558eb3fc18a
Vitali Soitu <vitali.soitu@nitor.com>
2023-03-15 12:24:28 +0200

    Fix sense setting visibility when selecting for first time

--- src/components/SettingsComponent/SettingsComponent.js
+++ src/components/SettingsComponent/SettingsComponent.js
@@ -134,7 +134,7 @@ const SettingsComponent = ({ variant, classes }) => {
     const getValue = () => {
       if (category === 'mobility') {
         const val = options.find(option => settingsValues.mobility === option.id);
-        return val?.title;
+        return val?.title || null;
       }
       const list = options.filter(option => settingsValues[category].includes(option.id));
       return list.map(item => item.title);
@@ -151,7 +151,11 @@ const SettingsComponent = ({ variant, classes }) => {
         options={options}
         clearIcon={null}
         value={getValue()}
-        isOptionEqualToValue={option => settingsValues[category].includes(option.id)}
+        isOptionEqualToValue={option => (
+          category === 'mobility'
+            ? settingsValues[category] === option.id
+            : settingsValues[category].includes(option.id)
+        )}
         disableCloseOnSelect={!isSingleOption}
         getOptionLabel={option => option.title || option}
         onKeyDown={keyboardHandler(e => handleKeyboardSelect(label, category, e), ['space', 'enter', 'up', 'down'])}
