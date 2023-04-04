e2d49a058054c191b91db9c8f21ff20a0d4d22a3
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-04 19:31:24 +0300

    Button for own settings

---
+++ src/components/OwnSettingsMenuButton/index.js
@@ -0,0 +1,5 @@
+import { withStyles } from '@mui/styles';
+import OwnSettingsMenuButton from './OwnSettingsMenuButton';
+import styles from './styles';
+
+export default withStyles(styles)(OwnSettingsMenuButton);
