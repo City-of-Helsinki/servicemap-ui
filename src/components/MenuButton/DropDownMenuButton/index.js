04ebd3e4cb7e09a3f7ef25a7f79303081fa92b6b
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-14 08:17:54 +0300

    Deduplicate styles

--- src/components/MenuButton/DropDownMenuButton/index.js
+++ src/components/MenuButton/DropDownMenuButton/index.js
@@ -1,5 +1,5 @@
 import { withStyles } from '@mui/styles';
 import DropDownMenuButton from './DropDownMenuButton';
-import styles from './styles';
+import styles from '../styles';
 
 export default withStyles(styles)(DropDownMenuButton);
