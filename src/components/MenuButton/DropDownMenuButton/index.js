4b474be0d33eb3062466089ccfd9e5b899cfad6c
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-14 09:49:57 +0300

    Fix style imports

--- src/components/MenuButton/DropDownMenuButton/index.js
+++ src/components/MenuButton/DropDownMenuButton/index.js
@@ -1,5 +1,3 @@
-import { withStyles } from '@mui/styles';
 import DropDownMenuButton from './DropDownMenuButton';
-import styles from '../styles';
 
-export default withStyles(styles)(DropDownMenuButton);
+export default DropDownMenuButton;
--- src/components/MenuButton/MenuButton/index.js
+++ src/components/MenuButton/MenuButton/index.js
@@ -1,6 +1,6 @@
 import { withStyles } from '@mui/styles';
 import { injectIntl } from 'react-intl';
 import MenuButton from './MenuButton';
-import styles from '../styles';
+import styles from './styles';
 
 export default injectIntl(withStyles(styles)(MenuButton));
