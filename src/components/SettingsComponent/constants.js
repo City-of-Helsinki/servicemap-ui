99c66e621377dae16f933b82c53883a8e26dfbaa
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-14 17:26:47 +0300

    Deduplicate option selection code

--- src/components/SettingsComponent/constants.js
+++ src/components/SettingsComponent/constants.js
@@ -1,3 +1,7 @@
+import {
+  setMapType, setMobility, toggleCity, toggleColorblind, toggleHearingAid, toggleVisuallyImpaired,
+} from '../../redux/actions/settings';
+
 const senses = ['colorblind', 'hearingAid', 'visuallyImpaired'];
 
 export default {
@@ -23,4 +27,43 @@ export default {
     cities: Object.keys(settings.cities)
       .filter(city => settings.cities[city] === true),
   }),
+  // I am sorry. This was done to deduplicate code. Not pretty.
+  handleOptionSelecting: (id, category, dispatch, cities, settingsValues) => {
+    if (!id) {
+      return;
+    }
+    if (category === 'mobility') {
+      dispatch(setMobility(id));
+    }
+    if (category === 'cities') {
+      const settingObj = cities;
+      settingObj[id] = !settingObj[id];
+      dispatch(toggleCity(settingObj));
+    }
+
+    if (category === 'senses') {
+      if (id === 'hearingAid') {
+        dispatch(toggleHearingAid());
+      }
+      // settingsValues.senses contains all previous sense settings. So now if it does not include
+      // "id" then it was turned on just now.
+      const settingTurnedOn = !settingsValues.senses.includes(id);
+      if (id === 'colorblind') {
+        dispatch(toggleColorblind());
+        if (settingTurnedOn) {
+          dispatch(setMapType('accessible_map'));
+        } else if (!settingsValues.senses.includes('visuallyImpaired')) {
+          dispatch(setMapType('servicemap'));
+        }
+      }
+      if (id === 'visuallyImpaired') {
+        dispatch(toggleVisuallyImpaired());
+        if (settingTurnedOn) {
+          dispatch(setMapType('accessible_map'));
+        } else if (!settingsValues.senses.includes('colorblind')) {
+          dispatch(setMapType('servicemap'));
+        }
+      }
+    }
+  },
 };
