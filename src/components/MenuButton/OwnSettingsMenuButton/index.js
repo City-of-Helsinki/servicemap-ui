5a342c876c4d9ba32a2cb0cf969d73b2c41506e2
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-14 08:17:54 +0300

    Deduplicate styles

--- src/components/MenuButton/OwnSettingsMenuButton/index.js
+++ src/components/MenuButton/OwnSettingsMenuButton/index.js
@@ -1,5 +1,5 @@
 import { withStyles } from '@mui/styles';
 import OwnSettingsMenuButton from './OwnSettingsMenuButton';
-import styles from './styles';
+import styles from '../styles';
 
 export default withStyles(styles)(OwnSettingsMenuButton);
