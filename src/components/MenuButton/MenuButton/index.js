b44f4607658aea02f6395087b5d23f4f392d50b2
Vitali Soitu <vitali.soitu@nitor.com>
2023-04-15 17:42:01 +0300

    Fix focus when closing download dialog

--- src/components/MenuButton/MenuButton/index.js
+++ src/components/MenuButton/MenuButton/index.js
@@ -1,6 +1,5 @@
 import { withStyles } from '@mui/styles';
-import { injectIntl } from 'react-intl';
 import MenuButton from './MenuButton';
 import styles from './styles';
 
-export default injectIntl(withStyles(styles)(MenuButton));
+export default withStyles(styles)(MenuButton);
