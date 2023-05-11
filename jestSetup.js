7a0e25616700fb292ac0484b471bac5f7d91930a
Vitali Soitu <vitali.soitu@nitor.com>
2023-05-11 09:48:00 +0300

    Remove enzyme

--- jestSetup.js
+++ jestSetup.js
@@ -1,8 +1,3 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
 // Prevent jest test passing if Failed prop type error occures
 const originalConsoleError = console.error;
 console.error = message => {
