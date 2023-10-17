b7a0273a9a1b07e082402ade5653a7bf3b3cccdc
Vitali Soitu <vitali.soitu@nitor.com>
2023-10-17 21:11:47 +0300

    Memoize city and organization settings

--- src/redux/selectors/settings.js
+++ src/redux/selectors/settings.js
@@ -1,1 +1,16 @@
+import { createSelector } from 'reselect';
+import config from '../../../config';
+
 export const getCitySettings = state => state.settings.cities;
+const selectCities = state => state.settings.cities;
+const selectOrganizations = state => state.settings.organizations;
+
+export const selectSelectedCities = createSelector(
+  [selectCities],
+  cities => config.cities.filter(c => cities[c]),
+);
+
+export const selectSelectedOrganizations = createSelector(
+  [selectOrganizations],
+  organizations => config.organizations?.filter(org => organizations[org.id]),
+);
