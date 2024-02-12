fe474e068df769b69ba52f1b585bed4121b8a2c4
Vitali Soitu <vitali.soitu@nitor.com>
2024-02-12 14:42:39 +0200

    refactor selectors

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,24 +1,3 @@
-import { connect } from 'react-redux';
-import fetchSearchResults from '../../redux/actions/search';
-import { changeSelectedUnit } from '../../redux/actions/selectedUnit';
 import SearchBarComponent from './SearchBarComponent';
 
-// Listen to redux state
-const mapStateToProps = (state) => {
-  const {
-    navigator, searchResults,
-  } = state;
-  const { isFetching, previousSearch } = searchResults;
-  return {
-    previousSearch,
-    isFetching,
-    navigator,
-  };
-};
-
-export const SearchBar = connect(
-  mapStateToProps,
-  { changeSelectedUnit, fetchSearchResults },
-)(SearchBarComponent);
-
-export default SearchBar;
+export default SearchBarComponent;
