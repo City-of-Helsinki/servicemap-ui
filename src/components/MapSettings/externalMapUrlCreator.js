5b3c0749bde02151712daaf374ee19454b1bfef8
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2024-06-20 13:29:12 +0300

    Add Espoo and Vantaa 3D-map links to map settings

--- src/components/MapSettings/externalMapUrlCreator.js
+++ src/components/MapSettings/externalMapUrlCreator.js
@@ -1,3 +1,5 @@
+import proj4 from 'proj4';
+
 const calculateDistance = (mapType, zoom) => {
   if (mapType === 'ortographic') {
     const exponent = 3 - zoom;
@@ -18,6 +20,12 @@ const calculateDistance = (mapType, zoom) => {
   return 0;
 };
 
+const transformCoordsToEPSG3879 = (lng, lat) => {
+  const EPSG3879 = '+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
+  const EPSG4326 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+  return proj4(EPSG4326, EPSG3879, [lng, lat]);
+};
+
 class ExternalMapUrlCreator {
   /**
    *
@@ -28,7 +36,7 @@ class ExternalMapUrlCreator {
    * @param lang locale in what the map should be opened
    * @returns url to kartta.hel.fi/3d
    */
-  static create3DMapUrl(lng, lat, zoom, mapType, lang) {
+  static createHelsinki3DMapUrl(lng, lat, zoom, mapType, lang) {
     const distance = calculateDistance(mapType, zoom);
     // Leaflet zoom levels represent a zoom by power of 2. Magic number 52800 was calculated by
     // measuring Malmi runway with ruler and testing "distance" param until a fit was found.
@@ -42,6 +50,17 @@ class ExternalMapUrlCreator {
     params.append('heading', '360.00');
     params.append('roll', '0.00');
     return `https://kartta.hel.fi/3d/?${params.toString()}`;
+  }
+
+  static createEspoo3DMapUrl(lng, lat, lang) {
+    const [x, y] = transformCoordsToEPSG3879(lng, lat);
+    return `https://kartat.espoo.fi/IMS/${lang}/Map?layers=Kaupunkimalli&cp=${y},${x}`;
+  }
+
+  static createVantaa3DMapUrl(lang) {
+    const params = new URLSearchParams();
+    params.append('locale', lang);
+    return `https://vantaa.kunta3d.fi/Map.html?${params.toString()}`;
   }
 }
 
