fc5a397fd7ffb4f0255e6fbdd74d52a049484fd7
mikkojamG <mikko.jamia@gofore.com>
2024-11-19 13:53:47 +0200

    fix: bottomNav settings add close button PL-18

--- src/components/MobileSettingsHeader/MobileSettingsHeader.js
+++ src/components/MobileSettingsHeader/MobileSettingsHeader.js
@@ -1,14 +1,29 @@
-import { FormLabel } from '@mui/material';
+import { FormLabel, IconButton } from '@mui/material';
+import { Close } from '@mui/icons-material';
 import styled from '@emotion/styled';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 
-const MobileSettingsHeader = ({ textId }) => (
-  <StyledFormLabel>
-    <FormattedMessage id={textId} />
-  </StyledFormLabel>
-);
+function MobileSettingsHeader({ textId, onClose }) {
+  const intl = useIntl();
+
+  return (
+    <StyledWrapper>
+      {onClose && (
+        <StyledCloseButton
+          onClick={onClose}
+          aria-label={intl.formatMessage({ id: 'general.close' })}
+        >
+          <Close />
+        </StyledCloseButton>
+      )}
+      <StyledFormLabel>
+        <FormattedMessage id={textId} />
+      </StyledFormLabel>
+    </StyledWrapper>
+  );
+}
 
 const StyledFormLabel = styled(FormLabel)(() => ({
   fontWeight: 700,
@@ -18,7 +33,28 @@ const StyledFormLabel = styled(FormLabel)(() => ({
   color: '#000',
 }));
 
+const StyledWrapper = styled('div')(() => ({
+  display: 'flex',
+  alignItems: 'flex-start',
+  justifyContent: 'space-between',
+  marginBottom: '1rem',
+}));
+
+const StyledCloseButton = styled(IconButton)(() => ({
+  order: 1,
+  padding: 0,
+  svg: {
+    fill: '#000',
+  },
+}));
+
 MobileSettingsHeader.propTypes = {
   textId: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
 };
+
+MobileSettingsHeader.defaultProps = {
+  onClose: null,
+};
+
 export default MobileSettingsHeader;
