7260df9525b24559538cfb94acc17d8489aa7a35
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2024-11-29 11:42:58 +0200

    Improve fetchUnits to increase search radius when necessary
    By default, fetch units within 100 meters but if there are less than 50
    units, fetch units within 500 meters. 50 units is the maximum amount
    that is currently wanted to be shown in the map.

--- src/views/AddressView/utils/fetchAddressUnits.js
+++ src/views/AddressView/utils/fetchAddressUnits.js
@@ -1,14 +1,15 @@
 import { unitsFetch } from '../../../utils/fetch';
 
-const fetchAddressUnits = async (lnglat) => {
+const fetchAddressUnits = async (lnglat, distance) => {
+  console.log('fetchAddressUnits');
   const options = {
     lat: `${lnglat[1]}`,
     lon: `${lnglat[0]}`,
-    distance: 500,
+    distance: distance,
     only: 'name,location,accessibility_shortcoming_count,',
     geometry: false,
     page: 1,
-    page_size: 200,
+    page_size: 50,
   };
 
   const unitData = await unitsFetch(options);
