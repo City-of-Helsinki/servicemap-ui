58bdf7e548fe0938c615807716a27e8ed2eb2eb8
mikkojamG <mikko.jamia@gofore.com>
2025-09-18 14:22:54 +0300

    chore: eventDetailView remove withRouter Ref: PL-89

--- src/views/EventDetailView/EventDetailView.js
+++ src/views/EventDetailView/EventDetailView.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { AccessTime, Event, Phone } from '@mui/icons-material';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import {
   DescriptionText,
@@ -23,13 +24,13 @@ function EventDetailView(props) {
     event = null,
     changeSelectedEvent,
     fetchSelectedUnit,
-    match,
     selectedUnit = null,
     map = null,
     intl,
     embed = false,
   } = props;
 
+  const params = useParams();
   const isMobile = useMobileStatus();
   const getLocaleText = useLocaleText();
   const [centered, setCentered] = useState(false);
@@ -90,7 +91,7 @@ function EventDetailView(props) {
   useEffect(() => {
     // TODO: move this first fetch to server side
     if (!event) {
-      if (match.params && match.params.event) {
+      if (params && params.event) {
         const options = {
           include: 'location,location.accessibility_shortcoming_count',
         };
@@ -119,14 +120,7 @@ function EventDetailView(props) {
             }
           }
         };
-        eventFetch(
-          options,
-          onStart,
-          onSuccess,
-          onError,
-          null,
-          match.params.event
-        );
+        eventFetch(options, onStart, onSuccess, onError, null, params.event);
       }
     } else if (!selectedUnit || event.location.id !== selectedUnit.id) {
       // Attempt fetching selected unit if it doesn't exist or isn't correct one
@@ -258,7 +252,6 @@ EventDetailView.propTypes = {
   event: PropTypes.objectOf(PropTypes.any),
   fetchSelectedUnit: PropTypes.func.isRequired,
   map: PropTypes.objectOf(PropTypes.any),
-  match: PropTypes.objectOf(PropTypes.any).isRequired,
   intl: PropTypes.objectOf(PropTypes.any).isRequired,
   selectedUnit: PropTypes.objectOf(PropTypes.any),
 };
